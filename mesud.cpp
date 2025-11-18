3e809354e98cdf7accd031afceaa005b14d21037
mesudsuleyman068-afk <mesudsuleyman068@gmail.com>
2025-11-18 23:47:43 +0300

    Update prompts (spelling correction).cpp for clarity

--- mesud.cpp
+++ mesud.cpp
@@ -2,11 +2,12 @@
 
 int main() {
     int num1, num2, sum;
-    std::cout << "Enter the first number: ";
+    std::cout << "Enter the first num: ";
     std::cin >> num1;
-    std::cout << "Enter the second number: ";
+    std::cout << "Enter the second num: ";
     std::cin >> num2;
     sum = num1 + num2;
     std::cout << "The sum is: " << sum << std::endl;
     return 0;
+
 }
